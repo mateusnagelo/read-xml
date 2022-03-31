0fe480fea2461fca72a7b2ae995f3b319220f25b
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-03-31 11:58:12 -0300

    Finalizado area do input file e inserido o footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// FAZER REQUISICAO DO JSON  MOSTRAR NA TELA COMO TABELA
 
     const mostrar = async ()=>{
         const req = await fetch("nf.json");
@@ -7,17 +8,22 @@
         const c = (elem) => {
             return document.getElementById(elem);
         }
-
+        
         c('tableItems').style.display = 'block';
         c('headTab').style.display = 'block';
         c('file').style.display = 'none';
+        c('btn').style.display = 'none';
+        c('label').style.display = 'none';
+        c('small').style.display = 'none';
 
         c('chave').innerHTML = 'Chave de acesso:'+ ' ' + xml.chNFe;
         c('cnpj').innerHTML = 'CNPJ:'+ ' ' +xml.cnpj;
         c('nnf').innerHTML = 'Número NF:'+ ' ' +xml.nnf;
         c('fantasia').innerHTML = 'Nome fantasia:'+ ' ' +xml.xfant;
 
+        
         document.querySelector('.areaButtons').style.position = 'relative';
+        document.getElementById('footer').style.position = 'relative';
 
         const xmlItems = xml['xmlItem'];
         let html ='<thead class="thead-light"><tr><th scope="col">Item</th><th scope="col">Código</th><th scope="col">Código de barras</th><th scope="col">Descrição</th><th scope="col">Preço Un</th><th scope="col">NCM</th><th scope="col">CEST</th><th scope="col">CFOP</th><th scope="col">CST/CSOSN</th><th scope="col">%ICMS</th><th scope="col">%RED.ICMS</th><th scope="col">%RED.ICMSST</th><th scope="col">PIS/COFINS<br>Venda</th><th scope="col">PIS/COFINS<br>Compra</th><th scope="col">%IPI</th></tr></thead>';
@@ -65,7 +71,10 @@
             c('tabela').innerHTML = html;
             
          }); 
-}
+};
+
+// FUNCTION PARA LIMPAR A TELA E VOLTAR PARA NOVA CONSULTA
+
 
 function voltar(){
     document.getElementById('impostos').style.display = 'none';
@@ -74,20 +83,73 @@ function voltar(){
             document.getElementById('file').style.display = 'inline-block'
         }
 
+    document.getElementById('btn').style.display;
+        if(document.getElementById('btn').style.display == 'none'){
+            document.getElementById('btn').style.display = 'inline-block'
+        }
     document.querySelector('.areaButtons').style.position;
         if(document.querySelector('.areaButtons').style.position == 'relative'){
             document.querySelector('.areaButtons').style.position = 'fixed';
         }
-}
+    document.getElementById('footer').style.position;
+        if(document.getElementById('footer').style.position == 'relative'){
+            document.getElementById('footer').style.position = 'absolute';
+        }
+    document.getElementById('label').style.display;
+        if(document.getElementById('label').style.display == 'none'){
+            document.getElementById('label').style.display = 'inline-block'
+        }
+    document.getElementById('small').style.display;
+        if(document.getElementById('small').style.display == 'none'){
+            document.getElementById('small').style.display = 'inline-block'
+        }
+};
+
+
+// MENU LATERAL - ASIDE
 
 const c = (elem) => {
     return document.getElementById(elem);
 }
 
 function abrirMenu(){
-    if(c('linksUteis').style.width == '350px'){
+    if(c('linksUteis').style.width == '400px'){
         c('linksUteis').style.width = '0px';
     }else{
-        c('linksUteis').style.width = '350px';
+        c('linksUteis').style.width = '400px';
     }
-}
+};
+
+// CONTROLAR BOTAO PARA SCROLLAR PARA CIMA
+
+
+// FECHAR MENU AO SCROLLAR 
+
+function fecharMenu(){
+    if(window.scrollY > 0){
+        c('linksUteis').style.width = '0px';
+    }
+};
+
+window.addEventListener('scroll', fecharMenu);
+
+
+
+function subirTela(){
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth'
+    });
+};
+
+function ocultarBotao(){
+    if(window.scrollY > 300){
+        document.querySelector('.btnToTop').style.display = 'block';
+    }else{
+        document.querySelector('.btnToTop').style.display = 'none';
+    }
+};
+
+window.addEventListener('scroll', ocultarBotao);
+
+
