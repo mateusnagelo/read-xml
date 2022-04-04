1f4ce9dddc84999ce03a06c5049060a126afe68a
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-04-04 11:27:36 -0300

    Corrigido eventListener de click e feito alguns ajustes dentro da table

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// FAZER REQUISICAO DO JSON  MOSTRAR NA TELA COMO TABELA
+// FAZENDO REQUISICAO NO BACKEND
 
-const mostrar = async () => {
+function mostrar() {
 
     const inputFile = document.getElementById("file");
     const btnUpload = document.getElementById("btn");
     
-
-    btnUpload.addEventListener("click", function () {
         const xhr = new XMLHttpRequest();
         const formData = new FormData();
         for (const file of inputFile.files) {
@@ -17,116 +15,89 @@ const mostrar = async () => {
         xhr.addEventListener('readystatechange', function () {
             if (xhr.readyState === 4 && xhr.status == 200) {
                 var json = JSON.parse(xhr.responseText);
-                  
-            
-            
-            // const req = await fetch("nf.json");
-    const xml = json
+                const xml = json
+                
+                document.getElementById('impostos').style.display = 'block';
+                const c = (elem) => {
+                    return document.getElementById(elem);
+                }
 
-    console.log("Teste: "+xml)
+                c('tableItems').style.display = 'block';
+                c('headTab').style.display = 'block';
+                c('file').style.display = 'none';
+                c('btn').style.display = 'none';
+                c('label').style.display = 'none';
+                c('small').style.display = 'none';
+                c('linksUteis').style.width = '0px';
+                c('btn2').style.display = 'inline-block';
+                c('btn3').style.display = 'inline-block';
 
-    // console.log(xml['xmlItem']);
-    document.getElementById('impostos').style.display = 'block';
-    const c = (elem) => {
-        return document.getElementById(elem);
-    }
-
-    c('tableItems').style.display = 'block';
-    c('headTab').style.display = 'block';
-    c('file').style.display = 'none';
-    c('btn').style.display = 'none';
-    c('label').style.display = 'none';
-    c('small').style.display = 'none';
-    c('linksUteis').style.width = '0px';
-    c('btn2').style.display = 'inline-block';
-    c('btn3').style.display = 'inline-block';
-
-    c('chave').innerHTML = 'Chave de acesso:' + ' ' + xml.chNFe;
-    c('cnpj').innerHTML = 'CNPJ:' + ' ' + xml.cnpj;
-    c('nnf').innerHTML = 'Número NF:' + ' ' + xml.nnf;
-    c('fantasia').innerHTML = 'Nome fantasia:' + ' ' + xml.xfant;
+                c('chave').innerHTML = 'Chave de acesso:' + ' ' + xml.chNFe;
+                c('cnpj').innerHTML = 'CNPJ:' + ' ' + xml.cnpj;
+                c('nnf').innerHTML = 'Número NF:' + ' ' + xml.nnf;
+                c('fantasia').innerHTML = 'Nome fantasia:' + ' ' + xml.xfant;
 
 
-    document.querySelector('.areaButtons').style.position = 'relative';
-    document.getElementById('footer').style.position = 'relative';
+                document.querySelector('.areaButtons').style.position = 'relative';
+                document.getElementById('footer').style.position = 'relative';
 
-    const xmlItems = xml['xmlItem'];
-    let html = '<thead class="thead-light"><tr><th scope="col">Item</th><th scope="col">Código</th><th scope="col">Código de barras</th><th scope="col">Descrição</th><th scope="col">Preço Un</th><th scope="col">NCM</th><th scope="col">CEST</th><th scope="col">CFOP</th><th scope="col">CST</th><th scope="col">%ICMS</th><th scope="col">%RED.ICMS</th><th scope="col">%RED.ICMSST</th><th scope="col">PIS/COFINS<br>Venda</th><th scope="col">PIS/COFINS<br>Compra</th><th scope="col">%IPI</th></tr></thead>';
+                const xmlItems = xml['xmlItem'];
+                let html = '<thead class="thead-light"><tr><th scope="col">Item</th><th scope="col">Código</th><th scope="col">Código de barras</th><th scope="col">Descrição</th><th scope="col">Preço Un</th><th scope="col">NCM</th><th scope="col">CEST</th><th scope="col">CFOP</th><th scope="col">CST</th><th scope="col">%ICMS</th><th scope="col">%RED.ICMS</th><th scope="col" id="tdNone2">%RED.ICMSST</th><th scope="col">PIS/COFINS<br>Venda</th><th scope="col">PIS/COFINS<br>Compra</th><th scope="col">%IPI</th></tr></thead>';
 
-    xmlItems.map(function (item, index) {
+                xmlItems.map(function (item, index) {
 
-        // CRIANDO CONDIÇÃO PARA PREENCHER O PIS E COFINS DE COMPRA
-        let pisC = '';
+                    // CRIANDO CONDIÇÃO PARA PREENCHER O PIS E COFINS DE COMPRA
 
-        if (item.cstPis == "01") {
-            pisC = "50";
-        } else if (item.cstPis == "06") {
-            pisC = "73";
-        } else if (item.cstPis == "04") {
-            pisC = "70";
-        }
+                    let pisC = '';
 
-        let bcIcms = item.pIcms;
-        let bcIcmsInt = parseInt(bcIcms);
+                    if (item.cstPis == "01") {
+                        pisC = "50";
+                    } else if (item.cstPis == "06") {
+                        pisC = "73";
+                    } else if (item.cstPis == "04") {
+                        pisC = "70";
+                    } else if (item.cstPis == "08"){
+                        pisC = "***";
+                    }
 
-        let codigo = item.cProd.replace(/^(0+)(\d)/g, "$2");
+                    let bcIcms = item.pIcms;
+                    let bcIcmsInt = parseInt(bcIcms);
 
-        let preco = item.preco;
-        let precoInt = parseFloat(preco);
+                    let codigo = item.cProd.replace(/^(0+)(\d)/g, "$2");
 
+                    let preco = item.preco;
+                    let precoInt = parseFloat(preco);
 
-        html += '<tr class="linhaTable">';
-        html += '<td>' + item.nitem + '</td>';
-        html += '<td>' + codigo + '</td>';
-        html += '<td>' + item.cEAN + '</td>';
-        html += '<td>' + item.xprod + '</td>';
-        html += '<td>' + 'R$ ' + precoInt.toFixed(2) + '</td>';
-        html += '<td>' + item.ncm + '</td>';
-        html += '<td>' + item.cest + '</td>';
-        html += '<td>' + item.cfop + '</td>';
-        html += '<td>' + item.cstCson + '</td>';
-        html += '<td>' + bcIcmsInt + ' %' + '</td>';
-        html += '<td>' + item.pRedBC + '%' + '</td>';
-        html += '<td>' + item.pRedBCST + '</td>';
-        html += '<td>' + item.cstPis + '</td>';
-        // html += '<td>' +item.cstCofins+ '</td>';
-        html += '<td>' + pisC + '</td>';
-        html += '<td>' + item.cstIpi + '</td>';
-        html += '</tr>';
+                    
+                    let cest = item.cest
+                    if(cest === null){
+                        cest = 'Sem info'
+                    }
 
-        c('tabela').innerHTML = html;
+                    html += '<tr class="linhaTable">';
+                    html += '<td>' + item.nitem + '</td>';
+                    html += '<td>' + codigo + '</td>';
+                    html += '<td>' + item.cEAN + '</td>';
+                    html += '<td>' + item.xprod + '</td>';
+                    html += '<td>' + 'R$ ' + precoInt.toFixed(2) + '</td>';
+                    html += '<td>' + item.ncm + '</td>';
+                    html += '<td>' + cest + '</td>';
+                    html += '<td>' + item.cfop + '</td>';
+                    html += '<td>' + item.cstCson + '</td>';
+                    html += '<td>' + bcIcmsInt + ' %' + '</td>';
+                    html += '<td>' + item.pRedBC + '%' + '</td>';
+                    html += '<td id="tdNone">' + item.pRedBCST + '</td>';
+                    html += '<td>' + item.cstPis +' - '+ item.cstCofins +'</td>';
+                    html += '<td>' + pisC +' - '+ pisC +'</td>';
+                    html += '<td>' + item.cstIpi + '</td>';
+                    html += '</tr>';
 
-    });
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+                    c('tabela').innerHTML = html;
+
+                });
             }
-        });
-    })
-
-
-
-
-
-
-
-
-
-
-
-    
-};
-
+        })
+    }
 // FUNCTION PARA LIMPAR A TELA E VOLTAR PARA NOVA CONSULTA
 
 
@@ -137,10 +108,10 @@ function voltar() {
         document.getElementById('file').style.display = 'inline-block'
     }
 
-    document.getElementById('btn').style.display;
-    if (document.getElementById('btn').style.display == 'none') {
-        document.getElementById('btn').style.display = 'inline-block'
-    }
+    // document.getElementById('btn').style.display;
+    // if (document.getElementById('btn').style.display == 'none') {
+    //     document.getElementById('btn').style.display = 'inline-block'
+    // }
     document.querySelector('.areaButtons').style.position;
     if (document.querySelector('.areaButtons').style.position == 'relative') {
         document.querySelector('.areaButtons').style.position = 'fixed';
@@ -231,3 +202,16 @@ function openModal() {
     c('linksUteis').style.width = '0px';
 
 };
+
+
+const reloadBody = c('btn2')
+const reloadNav = c('btn3')
+
+reloadBody.addEventListener('click', () => {
+    location.reload()
+})
+
+
+reloadNav.addEventListener('click', () => {
+    location.reload()
+})
